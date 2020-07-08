0c37b260547b93d0dad258d1704924621c2e551a
Dmitry Kuzaev <dmitriikuzaev@yandex.ru>
2020-07-08 20:58:51 +0400

    Updates

---
+++ precache-manifest.97513b7045e84ee4a7d2df5b68711fe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2df7e7ded948df63f3868ec2e97d919",
+    "url": "/need-for-drive/index.html"
+  },
+  {
+    "revision": "375ba758a5a873b4fa86",
+    "url": "/need-for-drive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a8804614e6b8f6caa10",
+    "url": "/need-for-drive/static/js/2.5805f0fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/need-for-drive/static/js/2.5805f0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375ba758a5a873b4fa86",
+    "url": "/need-for-drive/static/js/main.9a63c534.chunk.js"
+  },
+  {
+    "revision": "d626b68bd1aca0cbff82",
+    "url": "/need-for-drive/static/js/runtime-main.7c67051a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/need-for-drive/static/media/logo.5d5d9eef.svg"
+  }
+]);
