568195a9157d3784f6c2d52c53e93c70ae9c1a90
Dmitry <dmitriikuzaev@yandex.ru>
2020-07-11 21:59:35 +0300

    Add base Main page

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main__sidebar">123</div>
+      <div className="main__body">123</div>
+    </div>
+  );
+};
+
+export default Main;
