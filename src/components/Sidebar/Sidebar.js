8f4715ddf0355f28c32b1d09954d67bb82c19e73
Dmitry <dmitriikuzaev@yandex.ru>
2020-07-12 18:09:40 +0300

    Add Sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+
+import { Burger } from "../../components";
+import "./Sidebar.scss";
+
+const Sidebar = ({ className, backdropClassName }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <div className={classNames("sidebar", className, { "sidebar--collapsed-false": !collapsed })}>
+      <div className="sidebar__main">
+        <div className="sidebar__burger">
+          <Burger onClick={toggleCollapsed} isClose={!collapsed} />
+        </div>
+      </div>
+      <div
+        className={classNames("sidebar__extra", {
+          "sidebar__extra--collapsed-false": !collapsed,
+        })}
+      >
+        Меню
+      </div>
+      <div
+        className={classNames(
+          "sidebar-backdrop",
+          { "sidebar-backdrop--collapsed-true": collapsed },
+          backdropClassName
+        )}
+      ></div>
+    </div>
+  );
+};
+
+export default Sidebar;
