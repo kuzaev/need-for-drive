ae8b90f72369f64ac6b18c616fccd90ac60cd55a
Dmitry <dmitriikuzaev@yandex.ru>
2020-07-12 18:54:14 +0300

    Add general export for components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Sidebar } from "./Sidebar";
+export { default as Burger } from "./Burger";
+export { default as SidebarMenu } from "./SidebarMenu";
+export { default as MainLink } from "./MainLink";
