d163334fb169b887d34f33dc44dc2ec019e1e2c0
Dmitry <dmitriikuzaev@yandex.ru>
2020-07-14 19:56:31 +0300

    Update Home component (add hero block)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,30 @@
 import React from "react";
 
+import { Header, Footer, MainLink } from "../../components";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="home__hero"></div>
+      <div className="home__hero hero">
+        <Header className="hero__header" />
+
+        <div className="hero__content">
+          <h1 className="hero__title">
+            <span className="hero__title-text">Каршеринг</span>
+            <span className="hero__title-text hero__title-text--green">Need for drive</span>
+          </h1>
+          <p className="hero__subtitle">Поминутная аренда авто твоего города</p>
+          <MainLink to="/order" className="hero__button" type="button">
+            Забронировать
+          </MainLink>
+        </div>
+
+        <Footer className="hero__footer" />
+      </div>
+
       <div className="home__slider"></div>
+      
     </div>
   );
 };
