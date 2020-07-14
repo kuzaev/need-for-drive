d3e268ec6f98c5848f8f4b2df663f72dd06ef320
Dmitry <dmitriikuzaev@yandex.ru>
2020-07-14 18:16:03 +0300

    Add GeoButton component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
+export { default as Button } from "./Button";
+export { default as Loader } from "./Loader";
 export { default as Sidebar } from "./Sidebar";
 export { default as Burger } from "./Burger";
 export { default as SidebarMenu } from "./SidebarMenu";
 export { default as MainLink } from "./MainLink";
 export { default as Home } from "./Home";
+export { default as GeoButton } from "./GeoButton";
