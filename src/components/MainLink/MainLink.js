2b631c731ea156b8667eab40eeec4168bc80b3ea
Dmitry <dmitriikuzaev@yandex.ru>
2020-07-14 18:47:19 +0300

    Update MainLink component

--- src/components/MainLink/MainLink.js
+++ src/components/MainLink/MainLink.js
@@ -4,14 +4,14 @@ import classNames from "classnames";
 
 import "./MainLink.scss";
 
-const MainLink = ({ children, to, className, exact, external }) => {
+const MainLink = ({ children, to, className, exact, external, type }) => {
   let match = useRouteMatch({ path: to, exact });
 
   if (external) {
     return (
       <a
         href={to}
-        className={classNames("link", className)}
+        className={classNames("link", className, { "link--button": type === "button" })}
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -22,8 +22,16 @@ const MainLink = ({ children, to, className, exact, external }) => {
 
   return (
     <React.Fragment>
-      <Link to={to} className={classNames("link", className, { "link--active": match })}>
-        {children}
+      <Link
+        to={to}
+        className={classNames(
+          "link",
+          className,
+          { "link--active": match },
+          { "link--button": type === "button" }
+        )}
+      >
+        {type === "button" ? <div className="link__inner">{children}</div> : children}
       </Link>
     </React.Fragment>
   );
